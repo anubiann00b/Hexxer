d62d7683c46c0eca59e2887383db538f89cff4f0
Shreyas <skraman1999@gmail.com>
2016-02-14 00:27:31 -0800

    First commit. Let's do this.

---
+++ desktop/src/me/shreyasr/hexxer/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package me.shreyasr.hexxer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import me.shreyasr.hexxer.Hexxer;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Hexxer(), config);
+	}
+}
